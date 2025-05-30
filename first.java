841458a7cbd1eaf4790815fb0b21a23374986de4
Aditya Bhure <adityabhure63@gmail.com>
2025-05-30 18:46:29 +0530

    Initial commit

---
+++ first.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class first{
+    public static void main(String args[]){
+        System.out.println("Hello Aditya ");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println(name);
+        
+    }
+}
