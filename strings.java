a2be61510b2fa2c6677323806b66828e91ed2024
Aditya Bhure <adityabhure63@gmail.com>
2025-06-20 11:19:48 +0530

    string

--- strings.java
+++ strings.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class strings {
     public static void main(String[] args) {
 
-       Scanner sc=new Scanner(System.in);
-       System.out.println("Enter The First Name ");
-       String firstN=sc.nextLine();
+    //    Scanner sc=new Scanner(System.in);
+    //    System.out.println("Enter The First Name ");
+    //    String firstN=sc.nextLine();
 
-       System.out.println("Enter The Last Name ");
-       String lastN=sc.nextLine();
+    //    System.out.println("Enter The Last Name ");
+    //    String lastN=sc.nextLine();
 
-       System.out.println(firstN+" "+lastN);
+    //    System.out.println(firstN+" "+lastN);
         
 
         // String firstName="Aditya";
@@ -47,5 +47,20 @@ public class strings {
         // }else{
         //     System.out.println("Strings are not equal");
         // }
+
+
+        String name="AdityaBhure";
+        System.out.println(name.length());
+        System.out.println(name.charAt(5));
+        System.out.println(name.substring(1,6));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+
+        String str="This,is,my,code";
+        String[]words=str.split(",");
+
+        for(String word:words){
+            System.out.println(word);
+        }
     }
 }
